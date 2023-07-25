4c4beb145f8b6044294a279c90cccfd766f20db8
raffaele-aurucci <raffaeleaurucci.informatica@gmail.com>
2023-07-25 18:48:53 +0200

    update scripts for move character and move camera, add children character in unity scene

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -5,64 +5,50 @@ using UnityEngine.Serialization;
 
 public class MoveCamera : MonoBehaviour
 {
+    public Transform playerTransform;
+    public float sensitivity = 150F;
 
-    public Transform playerPos;
+    private float _verticalRotation;
+    private float _horizontalRotation;
     
-    public float rotationSpeed = 3f;
     
-    // distanza camera dall'oggetto player
-    public float distance = 5f;
-    
-    // angolo di rotazione intorno all'asse X inizializzato a 180 gradi
-    private float currentX = 180f;
-    
-    // angolo di rotazione intorno all'asse Y
-    private float currentY = 0f;
-
-    private void Start() 
+    // Start is called before the first frame update
+    void Start()
     {
-        // Blocca e nasconde il cursore del mouse
-        Cursor.lockState = CursorLockMode.Locked;
+        // considera la rotazione verticale rispetto agli assi locali del parent
+        _verticalRotation = playerTransform.localEulerAngles.x;
+        
+        // considera la rotazione orizzontale rispetto agli assi globali
+        _horizontalRotation = playerTransform.eulerAngles.y;
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
         // Restituisce l'intensità dell'input del mouse sull'asse X.
         // Questo valore può essere positivo (da 0 a +1) se il mouse viene spostato verso dx
         // o negativo (da 0 a -1) se spostato verso sx.
-        currentX += Input.GetAxis("Mouse X") * rotationSpeed;
+        float mouseX = Input.GetAxis("Mouse X");
 
         // Restituisce l'intensità dell'input del mouse sull'asse Y.
         // Assume valore negativo verso l'alto (da 0 a -1) e positivo verso il basso (da 0 a 1)
-        currentY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        float mouseY = -Input.GetAxis("Mouse Y");
 
-        // Limita l'angolo di rotazione verticale (currentY) tra 0 gradi e 45 gradi utilizzando
-        // la funzione Mathf.Clamp.
-        // Se 0 < y < 45 lo restituisce, altrimenti se y < 0 restituisce 0, se y > 45 restituisce 45
-        currentY = Mathf.Clamp(currentY, 0, 45f);
+        // calcolo la rotazione verticale
+        _verticalRotation += mouseY * sensitivity * Time.deltaTime;
+        
+        // calcolo la rotazione orizzontale
+        _horizontalRotation += mouseX * sensitivity * Time.deltaTime;
+
+        // limito l'angolo di rotazione verticale
+        _verticalRotation = Mathf.Clamp(_verticalRotation, -10F, 30F);
+        
+        // applico la rotazione verticale alla camera lungo l'asse X
+        transform.localRotation = Quaternion.Euler(_verticalRotation, 0F, 0F);
+
+        // applico la rotazione orizzontale al giocatore lungo l'asse Y
+        playerTransform.rotation = Quaternion.Euler(0F, _horizontalRotation, 0F);
     }
+
     
-    // chiamato dopo Update()
-    private void LateUpdate()
-    {
-        // Crea un oggetto Quaternion a partire dagli angoli di rotazione attuali (currentY e currentX),
-        // per rappresentare la rotazione desiderata della telecamera.
-        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0f);
-        
-        // calcolo l'offset nel seguente modo:
-        // - (rotation * Vector3.forward) produce un vettore che punta nella direzione della vista desiderata dalla camera
-        // In Unity, quando si moltiplica un quaternion per un vettore, il quaternion viene utilizzato per ruotare il vettore.
-        // Quindi il vettore Vector3.forward è considerato ruotato di x gradi lungo l'asse X e y gradi lungo l'asse Y
-        // - distance specifica la distanza tra il player e la camera
-        
-        Vector3 offset = (rotation * Vector3.forward) * distance;
-        Vector3 desiredPosition = playerPos.position - offset;
-
-        // assegna la posizione alla telecamera
-        transform.position = desiredPosition;
-        
-        // punta all'oggetto target, ruota nella direzione dell'oggetto, senza di esso la camera mantiene il suo
-        // orientamento inziale
-        transform.rotation = rotation;
-    }
 }
--- Assets/Scripts/MoveCharacter.cs
+++ Assets/Scripts/MoveCharacter.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCharacter : MonoBehaviour {
-    
-    public float forceAmount = 1.0F;
-    
+public class MoveCharacter : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
     private Rigidbody _rigidbodyPlayer;
-    
-    public Transform cameraTransform;
-    
+
     void Start() 
     {
         _rigidbodyPlayer = GetComponent<Rigidbody>();
@@ -23,20 +21,14 @@ public class MoveCharacter : MonoBehaviour {
         // Restituisce l'intensità dell'input sull'asse verticale (-1 a 1) (W e S - freccia su e giu)
         float vertical = Input.GetAxis("Vertical");
 
-        // Calcolo la direzione di movimento rispetto all'orientamento della telecamera
+        // calcolo il movimento del giocatore
+        Vector3 movement = transform.forward * vertical + transform.right * horizontal;
+        movement.Normalize();
         
-        // - cameraTransform.forward è un vettore normalizzato della direzione in avanti della camera nello spazio globale
-        // - Scale() effettua la moltiplicazione componente per componente
-        // - normalized ottiene un vettore normalizzato di lunghezza 1
+        transform.position += movement * speed * Time.deltaTime;
         
-        // in sintesi questa operazione permette di ottenere la direzione in cui la telecamera è rivolta annullandone la
-        // componente verticale
-        Vector3 cameraForward = Vector3.Scale(cameraTransform.forward, new Vector3(1, 0, 1)).normalized;
-        
-        // somma i due vettori per ottenere la direzione di movimento normalizzata lungo l'asse orizzontale o verticale 
-        Vector3 moveDirection = (cameraForward * vertical + cameraTransform.right * horizontal).normalized;
-
-        // Applica la forza di movimento al rigidbody del personaggio
-        _rigidbodyPlayer.AddForce(moveDirection * forceAmount);
+        // setto i valori float a vertical e a horizontal per applicare l'animazione
+        animator.SetFloat("vertical", vertical);
+        animator.SetFloat("horizontal", horizontal);
     }
 }
